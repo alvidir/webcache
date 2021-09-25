6fa98189fdc2195df4175f2cd47402f0889f8908
Hèctor <thehector1593@gmail.com>
2021-09-25 12:57:58 +0200

    WIP: needed to apply changes over main function

--- README.md
+++ README.md
@@ -14,12 +14,6 @@ cache:
   methods:
     - GET # catch only responses of GET requests
 
-request:
-  timeout: 3000 # how much long can it takes a request to get back a response (ms)
-  methods:
-    - name: default
-      enabled: true # since non listed method are disabled by default, enable all them
-
 router:
   - endpoints: # afected endpoints for the current configuration (regex)
       - https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,32}/?$
@@ -36,6 +30,10 @@ router:
         enabled: false # block all POST requests (405 - method not allowed)
       - name: PUT
         enabled: false # block all PUT requests (405 - method not allowed)
+      - name: DELETE
+        cached: false # do not catch DELETE responses for the provided endpoints
+      - name: default
+        enabled: true # since non listed method are disabled by default, enable all them
 
     cached: true # enable caching for the provided endpoints
 ```
--- proxy.go
+++ proxy.go
@@ -10,22 +10,62 @@ import (
 	"time"
 )
 
+const (
+	ETAG_SERVER_HEADER = "ETag"
+	HTTP_CODE_BOUNDARY = 400
+)
+
 var (
 	ErrNotCached = fmt.Errorf("is not cached")
 	ErrNoContent = fmt.Errorf("has no content")
 )
 
-// Cache represents an storage for request's responses
+// A Cache represents an storage for request's responses
 type Cache interface {
 	Store(string, string, time.Duration)
 	Load(string) (string, error)
 }
 
+// A Config represents a set of settings about how the ReverseProxy must perform
 type Config interface {
+	IsEndpointAllowed(string) bool
 	IsMethodAllowed(string, string) bool
 	IsMethodCached(string, string) bool
+	ResponseTimeout(string) time.Duration
+	Headers(string, string) map[string]string
 }
 
+type httpMiddleware struct {
+	body   []byte
+	header int
+	w      http.ResponseWriter
+}
+
+// implement http.ResponseWriter
+// https://golang.org/pkg/net/http/#ResponseWriter
+func (cacher *httpMiddleware) Header() http.Header {
+	return cacher.w.Header()
+}
+
+func (cacher *httpMiddleware) Write(b []byte) (int, error) {
+	cacher.body = append(cacher.body, b...)
+	return cacher.w.Write(b)
+}
+
+func (cacher *httpMiddleware) WriteHeader(i int) {
+	cacher.header = i
+	cacher.w.WriteHeader(i)
+}
+
+func newHttpMiddleware(w http.ResponseWriter) *httpMiddleware {
+	return &httpMiddleware{
+		body: make([]byte, 1024),
+		w:    w,
+	}
+}
+
+// A ReverseProxy is a cached reverse proxy that captures responses in order to provide it in the future instead of
+// permorming the request each time
 type ReverseProxy struct {
 	TargetURI   func(req *http.Request) (string, error)
 	HashRequest func(req *http.Request) (string, error)
@@ -34,6 +74,7 @@ type ReverseProxy struct {
 	cache       Cache
 }
 
+// NewReverseProxy returns a brand new ReverseProxy with the provided config and cache
 func NewReverseProxy(config Config, cache Cache) *ReverseProxy {
 	reverse := &ReverseProxy{
 		config: config,
@@ -43,18 +84,32 @@ func NewReverseProxy(config Config, cache Cache) *ReverseProxy {
 	return reverse
 }
 
+func (reverse *ReverseProxy) buildTag(host string, req *http.Request) (tag string, err error) {
+	tag = req.Header.Get("ETag")
+	if reverse.HashRequest != nil {
+		tag, err = reverse.HashRequest(req)
+		if err != nil {
+			log.Printf("[%s] TAG_ERROR %s - %s", req.Method, host, err.Error())
+			return "", err
+		}
+	}
+
+	tag = fmt.Sprintf("%s::%s::%s", req.Method, host, tag)
+	return
+}
+
 func (reverse *ReverseProxy) getSingleHostReverseProxy(host string) (*httputil.ReverseProxy, error) {
 	if v, ok := reverse.proxys.Load(host); ok {
 		if fn, ok := v.(*httputil.ReverseProxy); ok {
 			return fn, nil
 		}
 
-		log.Printf("got wrong type, want *httputil.ReverseProxy")
+		log.Printf("TYPE_ERROR %s - got wrong type, want *httputil.ReverseProxy", host)
 	}
 
 	remoteUrl, err := url.Parse(host)
 	if err != nil {
-		log.Println("target parse fail:", err)
+		log.Printf("URL_ERROR %s - %s", host, err.Error())
 		return nil, err
 	}
 
@@ -64,28 +119,67 @@ func (reverse *ReverseProxy) getSingleHostReverseProxy(host string) (*httputil.R
 }
 
 func (reverse *ReverseProxy) getCachedResponseBody(host string, req *http.Request) (string, error) {
-	if !reverse.config.IsMethodCached(host, req.Method) ||
-		reverse.HashRequest == nil {
+	if !reverse.config.IsMethodCached(host, req.Method) {
+		log.Printf("[%s] CACHE_MISS %s", req.Method, host)
 		return "", ErrNotCached
 	}
 
-	key, err := reverse.HashRequest(req)
+	tag, err := reverse.buildTag(host, req)
 	if err != nil {
 		return "", err
 	}
 
-	body, err := reverse.cache.Load(key)
+	body, err := reverse.cache.Load(tag)
 	if err != nil {
+		log.Printf("[%s] CACHE_MISS %s - %s", req.Method, host, err.Error())
 		return "", err
 	}
 
 	if len(body) == 0 {
+		log.Printf("[%s] CACHE_MISS %s", req.Method, host)
 		return "", ErrNoContent
 	}
 
+	log.Printf("[%s] CACHE_HIT %s", req.Method, host)
 	return body, nil
 }
 
+func (reverse *ReverseProxy) includeCustomHeaders(host string, req *http.Request) {
+	for key, value := range reverse.config.Headers(host, req.Method) {
+		req.Header.Add(key, value)
+	}
+}
+
+func (reverse *ReverseProxy) performHttpRequest(host string, w http.ResponseWriter, req *http.Request) error {
+	proxy, err := reverse.getSingleHostReverseProxy(host)
+	if err != nil {
+		log.Printf("[%s] PROXY_ERROR %s - %s", req.Method, host, err.Error())
+		return err
+	}
+
+	reverse.includeCustomHeaders(host, req)
+
+	middleware := newHttpMiddleware(w)
+	proxy.ServeHTTP(middleware, req)
+
+	if !reverse.config.IsMethodCached(host, req.Method) {
+		return nil
+	}
+
+	tag, err := reverse.buildTag(host, req)
+	if err != nil {
+		return nil
+	}
+
+	if middleware.header < HTTP_CODE_BOUNDARY {
+		timeout := reverse.config.ResponseTimeout(host)
+		reverse.cache.Store(tag, string(middleware.body), timeout)
+	}
+
+	return nil
+}
+
+// ServeHTTP performs http requests if not cached yet or returns the chaced body instead
 func (reverse *ReverseProxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	host := req.Host
 	if reverse.TargetURI != nil {
@@ -95,27 +189,23 @@ func (reverse *ReverseProxy) ServeHTTP(w http.ResponseWriter, req *http.Request)
 		}
 	}
 
-	if !reverse.config.IsMethodAllowed(host, req.Method) {
+	if !reverse.config.IsEndpointAllowed(host) {
 		w.Write([]byte("403: Host forbidden " + host))
 	}
 
+	if !reverse.config.IsMethodAllowed(host, req.Method) {
+		w.Write([]byte("405: Method not allowed " + host))
+	}
+
 	if body, err := reverse.getCachedResponseBody(host, req); err == nil {
-		log.Printf("[%s] CACHE_HIT %s", req.Method, host)
 		w.Write([]byte(body))
 		return
 	} else if err != ErrNotCached && err != ErrNoContent {
-		log.Printf("[%s] CACHE_MISS %s - %s", req.Method, host, err.Error())
 		w.Write([]byte("500: Internal server error"))
 		return
 	}
 
-	log.Printf("[%s] CACHE_MISS %s", req.Method, host)
-	proxy, err := reverse.getSingleHostReverseProxy(host)
-	if err != nil {
-		log.Printf("[%s] PROXY_ERROR %s - %s", req.Method, host, err.Error())
+	if err := reverse.performHttpRequest(host, w, req); err != nil {
 		w.Write([]byte("500: Internal server error"))
-		return
 	}
-
-	proxy.ServeHTTP(w, req)
 }
